9357ced3d3f75287555d07114e4a0e85d26b24b5
Jeroen F.J. Laros <jlaros@fixedpoint.nl>
2014-06-01 19:38:55 +0200

    Added vcf2wig subcommand.

--- scripts/cli.py
+++ scripts/cli.py
@@ -3,8 +3,9 @@
 """
 VCF manipulation toolkit.
 
-Copyright (c) 2013 Leiden University Medical Center <humgen@lumc.nl>
-Copyright (c) 2013 Jeroen F.J. Laros <j.f.j.laros@lumc.nl>
+
+Copyright (c) 2014 Leiden University Medical Center <humgen@lumc.nl>
+Copyright (c) 2014 Jeroen F.J. Laros <J.F.J.Laros@lumc.nl>
 
 Licensed under the MIT license, see the LICENSE file.
 """
@@ -12,6 +13,9 @@ Licensed under the MIT license, see the LICENSE file.
 from __future__ import division
 
 import argparse
+import os
+
+import wiggelen
 
 import vcf
 import vcf.utils as vcfutils
@@ -63,7 +67,35 @@ def diff(input_handles, output_handle, precision=10):
         value=symmetric_difference / total, precision=precision))
 #diff
 
+def vcf2wig(input_handle, output_handle, field="AF", prefix="",
+        function="min"):
+    """
+    Convert a VCF file to a wiggle track.
+
+    :arg input_handle: Open readable handle to a VCF file.
+    :type input_handle: stream
+    :arg output_handle: Open writable handle to a wiggle track.
+    :type output_handle: stream
+    """
+    def unpack(list_or_value):
+        if type(list_or_value) == list:
+            return func(list_or_value)
+        return list_or_value
+
+    reader = vcf.Reader(input_handle)
+    func = eval("lambda l: {}(l)".format(function))
+
+    wiggelen.write(map(lambda x: ("{}{}".format(prefix, x.CHROM), x.POS,
+        x.INFO.has_key(field) and unpack(x.INFO[field]) or 0.0), reader),
+        track=output_handle,
+        name=os.path.splitext(os.path.basename(output_handle.name))[0])
+#vcf2wig
+
 def main():
+    input_parser = argparse.ArgumentParser(add_help=False)
+    input_parser.add_argument("input_handle", metavar="INPUT",
+        type=argparse.FileType('r'), help="input file")
+
     pair_in_parser = argparse.ArgumentParser(add_help=False)
     pair_in_parser.add_argument("input_handles", metavar="INPUT", nargs=2,
         type=argparse.FileType('r'), help="pair of input files")
@@ -72,9 +104,13 @@ def main():
     output_parser.add_argument("output_handle", metavar="OUTPUT",
         type=argparse.FileType('w'), default='-', help="output file")
 
+    prefix_parser = argparse.ArgumentParser(add_help=False)
+    prefix_parser.add_argument("-x", dest="prefix", type=str, default="",
+        help='prefix for chromosome names (%(type)s default="%(default)s")')
+
     parser = argparse.ArgumentParser(
         formatter_class=argparse.RawDescriptionHelpFormatter,
-        )#description=usage[0], epilog=usage[1])
+        description=usage[0], epilog=usage[1])
     parser.add_argument('-v', action="version", version=version(parser.prog))
     subparsers = parser.add_subparsers()
 
@@ -83,6 +119,12 @@ def main():
     parser_diff.add_argument('-p', dest='precision', type=int, default=10,
         help='precision (%(type)s default=%(default)s)')
     parser_diff.set_defaults(func=diff)
+
+    parser_vcf2wig = subparsers.add_parser("vcf2wig", parents=[input_parser,
+        output_parser, prefix_parser], description=doc_split(vcf2wig))
+    parser_vcf2wig.add_argument('-f', dest='field', type=str, default="AF",
+        help='INFO field to convert (%(type)s default=%(default)s)')
+    parser_vcf2wig.set_defaults(func=vcf2wig)
 
     try:
         arguments = parser.parse_args()
